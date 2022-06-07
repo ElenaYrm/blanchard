452db0bc0aba9af7b49f5754f62a1d47fe4c47c5
elena_yaroma <yaroma.elena@gmail.com>
2022-06-07 19:43:42 +0400

    add layuot, style and js dropdown (header-bottom 1920px)

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// dropdown
+const menuBtn = document.querySelectorAll('.tab');
+const drops = document.querySelectorAll('.dropdown');
+
+menuBtn.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    let currentBtn = e.currentTarget;
+    let currentDrop = currentBtn.closest('.menu__item').querySelector('.dropdown');
+
+    menuBtn.forEach(function (el) {
+      if (el !== currentBtn) {
+        el.classList.remove('tab--active');
+        el.setAttribute("aria-expanded", "false");
+      }
+    });
+
+    drops.forEach(function (el) {
+      if (el !== currentDrop) {
+        el.classList.remove('dropdown--active');
+        el.setAttribute("aria-hidden", "true");
+      }
+    });
+
+    currentDrop.classList.toggle('dropdown--active');
+    currentDrop.setAttribute("aria-hidden", "false");
+    currentBtn.classList.toggle('tab--active');
+    currentBtn.setAttribute("aria-expanded", "true");
+  });
+});
+
+document.addEventListener('click', function (e) {
+  if (!e.target.closest('.menu__list')) {
+
+    menuBtn.forEach(function (el) {
+      el.classList.remove('tab--active');
+      el.setAttribute("aria-expanded", "false");
+    });
+
+    drops.forEach(function (el) {
+      el.classList.remove('dropdown--active');
+      el.setAttribute("aria-hidden", "true");
+    });
+  }
+});
